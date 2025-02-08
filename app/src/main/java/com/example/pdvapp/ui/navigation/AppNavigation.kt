0806eb4cc0e39e302e0b2c994534d15d11cd2a40
Alex <alexteotonio29@gmail.com>
2025-02-08 11:42:50 -0300

    Initializing project

--- app/src/main/java/com/example/pdvapp/ui/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/pdvapp/ui/navigation/AppNavigation.kt
@@ -33,6 +33,7 @@ fun AppNavigation() {
             )
         }
         composable("addProduct") {
+
             AddProductScreen(
                 onProductAdded = { navController.popBackStack() },
                 onCancel = { navController.popBackStack() }
